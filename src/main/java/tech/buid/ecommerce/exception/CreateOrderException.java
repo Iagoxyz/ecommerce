389a1dcd2e3ae939b0ff0c8c1281352f8396e6fa
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-09-09 09:31:10 -0300

    Exception - CreateOrderException

---
+++ src/main/java/tech/buid/ecommerce/exception/CreateOrderException.java
@@ -0,0 +1,8 @@
+package tech.buid.ecommerce.exception;
+
+public class CreateOrderException extends RuntimeException {
+
+    public CreateOrderException(String message) {
+        super(message);
+    }
+}
